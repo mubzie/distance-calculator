0b393ea4b3aadacac2808d8bb646df2772280942
mubzie <85705074+mubzie@users.noreply.github.com>
2023-07-09 09:44:19 +0100

    refactored the code by renaming variables and also formatted the print statement to output  result properly

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import inquirer
-from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
 from geopy.point import Point
+from geopy.distance import geodesic
 from simple_chalk import green, blue, yellow, red 
 
 
@@ -9,47 +9,37 @@ from simple_chalk import green, blue, yellow, red
 # Get user input for first city and country
 first_city = inquirer.prompt([
     inquirer.Text('city', message="what is the name of the first city"),
-    inquirer.Text('country', message="what is the name of the first country")
+    inquirer.Text('country', message="which country is the first city located")
 ])
 
 # Get user input for second city and country
 second_city = inquirer.prompt([
     inquirer.Text('city', message="what is the name of the second city"),
-    inquirer.Text('country', message="what is the name of the second country")
+    inquirer.Text('country', message="which country is the second city located")
 ])
 
 # Initialize the geopy Nominatim module
 geolocator = Nominatim(user_agent='distance calculator')
 
-# Get first city latitude and longitude
-first_loc_lat = geolocator.geocode(first_city).latitude
-first_loc_lon = geolocator.geocode(first_city).longitude
+# Getting the first city latitude and longitude using the geopy Nominatim module
+first_city_lat = geolocator.geocode(first_city).latitude
+first_city_lon = geolocator.geocode(first_city).longitude
 
-# Get the point between the lat and lon
-first_loc_point = Point(first_loc_lat, first_loc_lon)
+# Get the lat and lon point of the first city
+first_loc_point = Point(first_city_lat, first_city_lon)
 
-
-print(green(first_loc_lat))
-print(yellow(first_loc_lon))
-print(red(first_loc_point))
-
-# Get second city latitude and longitude
+# Getting second city latitude and longitude using the geopy Nominatim module
 second_loc_lat = geolocator.geocode(second_city).latitude
 second_loc_lon = geolocator.geocode(second_city).longitude
 
-# Get the point between the two cities
+# Get the lat and lon point of the second city
 second_loc_point = Point(second_loc_lat, second_loc_lon)
 
-
-print(green(second_loc_lat))
-print(yellow(second_loc_lon))
-print(red(second_loc_point))
-
-
-# Calculate the distance between the two cities
+# Get the distance between the two cities and return the result in kilometers
 distance = geodesic(first_loc_point, second_loc_point).kilometers
 
-# Calculating various distance for walking, bus, airplan, train, bicycle
+# Calculating various distance for walking, bus, airplane, train, bicycle,
+# using different measurement metric that correspond to each transportation mode
 walking = distance / 5
 bus = distance / 60
 airplane = distance / 800
@@ -65,13 +55,19 @@ trans_emoji = {
     airplane: '✈️ '
     }
 
-
+# printing out the result to the terminal
 print(green('\nResults:'))
 print(f'Distance between {first_city["city"]}, {first_city["country"]} and {second_city["city"]}, {second_city["country"]} by')
-print(yellow(f'{trans_emoji[walking]} Walking: {distance:.2f} Km, Time: {walking:.2f} hours'))
-print(yellow(f'{trans_emoji[bicycle]} Bicycle: {distance:.2f} Km, Time: {bicycle:.2f} hours'))
-print(yellow(f'{trans_emoji[bus]} Bus: {distance:.2f} Km, Time: {bus:.2f} hours'))
-print(yellow(f'{trans_emoji[train]} Train: {distance:.2f} Km, Time: {train:.2f} hours'))
-print(yellow(f'{trans_emoji[airplane]} Airplane: {distance:.2f} Km, Time: {airplane:.2f} hours'))
+print('-----------------------------------------------------')
+print(yellow(f'{trans_emoji[walking]} Walking: {distance:.2f} Km, Time(ETA): {walking:.2f} hours'))
+print('-----------------------------------------------------')
+print(yellow(f'{trans_emoji[bicycle]} Bicycle: {distance:.2f} Km, Time(ETA): {bicycle:.2f} hours'))
+print('-----------------------------------------------------')
+print(yellow(f'{trans_emoji[bus]} Bus: {distance:.2f} Km, Time(ETA): {bus:.2f} hours'))
+print('-----------------------------------------------------')
+print(yellow(f'{trans_emoji[train]} Train: {distance:.2f} Km, Time(ETA): {train:.2f} hours'))
+print('-----------------------------------------------------')
+print(yellow(f'{trans_emoji[airplane]} Airplane: {distance:.2f} Km, Time(ETA): {airplane:.2f} hours'))
+print('-----------------------------------------------------')
 
 
